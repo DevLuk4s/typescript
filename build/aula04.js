8a253f8f42a6207b61f287b0679102fc60e983ae
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-28 19:37:46 -0300

    aula04

---
+++ build/aula04.js
@@ -0,0 +1,7 @@
+"use strict";
+let nome;
+nome = "Lucas";
+let valor;
+valor = 10.5;
+console.log(nome);
+console.log(valor);
---
+++ src/aula04.ts
@@ -0,0 +1,8 @@
+let nome: string;
+nome = "Lucas";
+
+let valor: number;
+valor = 10.5;
+
+console.log(nome);
+console.log(valor);
