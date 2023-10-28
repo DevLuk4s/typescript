e5a36304679256199948d60d18ea865bbbdf67a0
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-28 20:03:00 -0300

    aula05

---
+++ build/aula05.js
@@ -0,0 +1,12 @@
+"use strict";
+// let vteste:string|number|boolean
+// vteste = "Lucas"
+// vteste = 13
+// vteste = true
+let cursos = ["JavaScript", "TypeScript", "Node.Js", "React"];
+let valores = [11, 7, 0, 4];
+cursos.push("Next.Js");
+valores.push(10);
+console.log(vteste);
+console.log(cursos);
+console.log(valores);
