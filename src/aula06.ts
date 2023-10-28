4e9cf8fd1b142b08b30d10ce2bdf2a3a92b01332
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-28 20:27:28 -0300

    aula06

---
+++ src/aula06.ts
@@ -0,0 +1,10 @@
+let numeros = [10, 20, 30, 40];
+numeros.push(50);
+numeros.unshift(5);
+numeros.pop();
+numeros.shift();
+
+console.log(numeros);
+
+let numeros__ro: ReadonlyArray<number> = [10, 20, 30, 40, 50];
+console.log(numeros__ro);
